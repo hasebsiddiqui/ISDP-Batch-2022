f03a86e43b55d9c87c6d48b378075d1f1136a9fa
Haseeb Ahmad <43096403+hasebsiddiqui@users.noreply.github.com>
2022-03-02 15:13:18 +0500

    EXPRESS CRUD Complete

---
+++ 15-express-crud/app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+let products = ["Shampoo", "Dove"];
+
+app.use(express.json());
+
+//default
+app.get("/", function (req, res) {
+  res.send("Hello World I am here");
+});
+
+//get all
+app.get("/api/products", function (req, res) {
+  res.send(products);
+});
+//getOne
+app.get("/api/products/:id", function (req, res) {
+  res.send(products[req.params.id]);
+});
+
+//Post data
+app.post("/api/products", function (req, res) {
+  var data = req.body;
+  products.push(data.name);
+  res.send(data.name);
+  //   res.send("Product created");
+});
+//Put data
+app.put("/api/products/:id", function (req, res) {
+  var data = req.body;
+  var productToEdit = products[req.params.id];
+
+  productToEdit = data.name;
+
+  products[req.params.id] = productToEdit;
+  res.send(productToEdit);
+  //   res.send("Product created");
+});
+//Delete data
+app.delete("/api/products/:id", function (req, res) {
+  var product = products[req.params.id];
+  products.splice(req.params.id, 1);
+
+  res.send(product);
+  //   res.send("Product created");
+});
+
+app.listen(8080);
