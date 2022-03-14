e35c057330cfe2f0c23ff39bfe92f970929246ab
Haseeb Ahmad <43096403+hasebsiddiqui@users.noreply.github.com>
2022-03-14 12:32:17 +0500

    JOI and Query

---
+++ 19 Req Query and JOI Validation/models/Product.js
@@ -0,0 +1,21 @@
+const Joi = require("joi");
+const mongoose = require("mongoose");
+
+var schema = mongoose.Schema({
+  name: String,
+  price: Number,
+  category: String,
+});
+
+const validateProduct = (data) => {
+  const joischema = Joi.object({
+    name: Joi.string().min(3).max(10),
+    price: Joi.number().min(0),
+  });
+  return joischema.validate(data, { abortEarly: false });
+};
+
+var Product = mongoose.model("Product", schema);
+
+module.exports.Product = Product;
+module.exports.validateProduct = validateProduct;
---
+++ 19 Req Query and JOI Validation/routes/api/products.js
@@ -0,0 +1,90 @@
+var express = require("express");
+var router = express.Router();
+const { Product } = require("../../models/Product");
+const { validateProduct } = require("../../models/Product");
+router.get("/", async function (req, res) {
+  try {
+    console.log(req.query);
+    let page = Number(req.query.page);
+    let perPage = Number(req.query.perPage);
+
+    page = (page - 1) * perPage;
+    console.log(page, perPage);
+
+    let result = await Product.find(req.body).skip(page).limit(perPage);
+
+    res.send(result);
+  } catch (err) {
+    console.log(err);
+    res.send(err.message);
+  }
+});
+router.get("/:id", async function (req, res) {
+  try {
+    let result = await Product.findById(req.params.id);
+    if (!result) {
+      res.status(400).send("Product with given ID not found");
+    }
+    res.send(result);
+  } catch (err) {
+    console.log(err);
+    // res.status(400).send(err.message);
+    res.status(400).send("The format of id is not correct");
+  }
+});
+router.post("/", async function (req, res) {
+  try {
+    let { error } = validateProduct(req.body);
+    if (error) {
+      res.status(400).send(error.message);
+    }
+
+    let result = new Product();
+    result.name = req.body.name;
+    result.price = req.body.price;
+    result = await result.save();
+    res.send(error);
+  } catch (err) {
+    console.log(err);
+    res.status(400).send(err.message);
+  }
+});
+router.put("/:id", async function (req, res) {
+  try {
+    let result = await Product.findById(req.params.id);
+    if (!result) {
+      res.status(400).send("The record with given id was not found");
+    }
+
+    //Another way to do this
+    // result.name = req.body.name;
+    // result.price = req.body.price;
+    // result = await result.save();
+
+    //{new:true} is an option to get updated data from
+    //findByIdAndUpdate function. Otherwise it will return old data.
+    result = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.send(result);
+  } catch (err) {
+    console.log(err);
+    res.status(400).send(err.message);
+  }
+});
+router.delete("/:id", async function (req, res) {
+  try {
+    let result = await Product.findById(req.params.id);
+    if (!result) {
+      res.status(400).send("record with given ID not found");
+    }
+    result = await Product.findByIdAndDelete(req.params.id);
+    res.send(result);
+  } catch (err) {
+    console.log(err);
+    res.status(400).send(err.message);
+    // res.status(400).send("The format of id is not correct");
+  }
+});
+
+module.exports = router;
